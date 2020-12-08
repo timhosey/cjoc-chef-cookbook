34be9ad67d7ddf6348a49ad09621b3e645d1b710
Tim Hosey <timhosey@tims-work-mbp.lan>
2020-12-08 13:37:16 -0800

    Added apt_repository, apt_update and apt_package resources to version_update.rb
    Signed-off-by: Tim Hosey <timhosey@tims-work-mbp.lan>

--- recipes/version_update.rb
+++ recipes/version_update.rb
@@ -1,1 +1,21 @@
+# Makes sure the correct apt repo is setup
+apt_repository 'add cjoc repo' do
+  uri 'https://downloads.cloudbees.com/cloudbees-core/traditional/client-master/rolling/debian'
+  components ['binary/']
+  action :add
+  key 'https://downloads.cloudbees.com/cloudbees-core/traditional/operations-center/rolling/debian/cloudbees.com.key'
+end
+
+# Updates apt
+apt_update 'update cjoc repo' do
+  ignore_failure true
+  action :periodic
+  frequency 21600
+end
+
 # Checks APT for a version change and upgrades if needed.
+apt_package 'cloudbees-core-oc' do
+  action :install
+  version node['cjoc']['target_version']
+end
+

46770b5ab208c365bbf59dda6acff2ee46ea2637
Tim Hosey <timhosey@tims-work-mbp.lan>
2020-12-09 15:40:08 -0800

    change version
    Signed-off-by: Tim Hosey <timhosey@tims-work-mbp.lan>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Tim Hosey'
 maintainer_email 'thosey@cloudbees.com'
 license 'All Rights Reserved'
 description 'Installs/Configures cjoc'
-version '0.1.1'
+version '0.2.0'
 chef_version '>= 15.0'
 
 depends 'chef-client'

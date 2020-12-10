789c0779b35686be232fd8d4883d2d8c1963b59c
Tim Hosey <timhosey@tims-work-mbp.lan>
2020-12-09 16:02:15 -0800

    removed logging, fixed package property missing
    Signed-off-by: Tim Hosey <timhosey@tims-work-mbp.lan>

--- resources/install_jenkins.rb
+++ resources/install_jenkins.rb
@@ -4,11 +4,9 @@ property :version, String, required: true
 property :package, String, required: true
 
 action :install_jenkins do
-  installed_version = `dpkg -s cloudbees-core-oc | grep -i version | sed 's/[A-Za-z\:\s-]//g'`.strip.gsub(/\s+/, "")
-  log "installed version is #{installed_version}" do
-    level :warn
-  end
-  
+  # Use bash to get current version of application
+  installed_version = `dpkg -s #{new_resource.package} | grep -i version | sed 's/[A-Za-z\:\s-]//g'`.strip.gsub(/\s+/, '')
+
   if installed_version != new_resource.version
     bash 'upgrade jenkins' do
       code <<-EOH

b41f0ac9a1fd837ba07e87a7bc8475e78ab946a1
Tim Hosey <thosey@cloudbees.com>
2021-01-07 13:14:14 -0800

    Added casc folder, added plugins.rb attribs, added CasC plugin
    Signed-off-by: Tim Hosey <thosey@cloudbees.com>

---
+++ attributes/plugins.rb
@@ -0,0 +1,10 @@
+plugins = {
+  'beer': {
+    'version': '1.2',
+    'source': 'https://updates.jenkins.io/download/plugins/beer/1.2/beer.hpi',
+  },
+  'configuration-as-code': {
+    'version': '1.46',
+    'source': 'https://updates.jenkins.io/download/plugins/configuration-as-code/1.46/configuration-as-code.hpi',
+  },
+}
--- recipes/plugin_update.rb
+++ recipes/plugin_update.rb
@@ -1,10 +1,3 @@
-plugins = {
-  'beer': {
-    'version': '1.2',
-    'source': 'https://updates.jenkins.io/download/plugins/beer/1.2/beer.hpi',
-  },
-}
-
 plugins.each do |plugin_name, value|
   cjoc_install_plugin "update/install #{plugin_name}" do
     version value[:version]

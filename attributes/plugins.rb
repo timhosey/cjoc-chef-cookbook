7a15243edbb4f26010bd998fee4e3d3a72e5a7ac
Tim Hosey <thosey@cloudbees.com>
2021-01-07 13:48:56 -0800

    Added master_plugins attrib
    Signed-off-by: Tim Hosey <thosey@cloudbees.com>

--- attributes/plugins.rb
+++ attributes/plugins.rb
@@ -5,6 +5,13 @@ default['cjoc_plugins'] = {
     'version': '1.2',
     'source': 'https://updates.jenkins.io/download/plugins/beer/1.2/beer.hpi',
   },
+}
+
+default['master_plugins'] = {
+  'beer': {
+    'version': '1.2',
+    'source': 'https://updates.jenkins.io/download/plugins/beer/1.2/beer.hpi',
+  },
   'configuration-as-code': {
     'version': '1.46',
     'source': 'https://updates.jenkins.io/download/plugins/configuration-as-code/1.46/configuration-as-code.hpi',

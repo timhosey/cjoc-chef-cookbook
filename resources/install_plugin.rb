1fd1b0be5634a51611d5b499a90f3b6eededf4d2
Tim Hosey <thosey@cloudbees.com>
2021-01-08 11:17:52 -0800

    Fixed rubytype for property of :restart
    Signed-off-by: Tim Hosey <thosey@cloudbees.com>

--- resources/install_plugin.rb
+++ resources/install_plugin.rb
@@ -2,7 +2,7 @@ property :version, String, required: true
 property :plugin, String, required: true
 property :plugin_source, String, required: true
 property :plugin_path, String, default: '/var/lib/cloudbees-core-oc/plugins'
-property :restart, Bool, default: false
+property :restart, Boolean, default: false
 
 # /var/lib/cloudbees-core-oc/plugins
 

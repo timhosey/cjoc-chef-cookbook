bf476d0c82518518e1aef5b6c09a52f42ff7fb15
Tim Hosey <thosey@cloudbees.com>
2021-01-14 10:31:21 -0800

    Added casc file loop to copy those files
    Signed-off-by: Tim Hosey <thosey@cloudbees.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,3 +6,6 @@ default['cjoc']['target_version'] = '2.263.1.2'
 
 # CJMC Version
 default['cjcm']['target_version'] = '2.263.1.2'
+
+# CJCM CasC path
+default['cjcm']['casc_path'] = '/etc/cbci/casc'
--- recipes/mm_config.rb
+++ recipes/mm_config.rb
@@ -14,3 +14,31 @@ service 'cloudbees-core-cm' do
   action :nothing
   reload_command 'systemctl daemon-reload'
 end
+
+# Copy CasC files; make dir if it doesn't exist
+directory '/etc/cbci/casc' do
+  action :create
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+end
+
+casc_files = [
+  'bundle.yaml',
+  'items.yaml',
+  'jenkins.yaml',
+  'plugin-catalog.yaml',
+  'plugins.yaml',
+  'rbac.yaml',
+]
+
+casc_files.each do |file|
+  cookbook_file "/etc/cbci/casc/#{file}" do
+    source "casc/#{file}"
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+  end
+end

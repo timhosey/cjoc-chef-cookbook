9ffb64782160812f2ad5f451f031c5323962894a
Tim Hosey <thosey@cloudbees.com>
2021-01-14 15:42:19 -0800

    incremented version to 0.4.0
    Signed-off-by: Tim Hosey <thosey@cloudbees.com>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Tim Hosey'
 maintainer_email 'thosey@cloudbees.com'
 license 'All Rights Reserved'
 description 'Installs/Configures cjoc'
-version '0.3.1'
+version '0.4.0'
 chef_version '>= 15.0'
 
 depends 'chef-client'

ba7a5974702666cf90cba9374ae7c6281179980f
Tim Hosey <thosey@cloudbees.com>
2021-01-26 11:03:30 -0800

    added begin-rescue block for plugin install
    Signed-off-by: Tim Hosey <thosey@cloudbees.com>

--- recipes/mm_plugin_update.rb
+++ recipes/mm_plugin_update.rb
@@ -1,14 +1,18 @@
-node['master_plugins'].each_with_index do |u, index|
-  plugin_name = u[1][:name]
-  plugin_ver = u[1][:version]
-  plugin_url = u[1][:source]
-  jenkins_restart = index == node['master_plugins'].size - 1
+begin
+  node['master_plugins'].each_with_index do |u, index|
+    plugin_name = u[1][:name]
+    plugin_ver = u[1][:version]
+    plugin_url = u[1][:source]
+    jenkins_restart = index == node['master_plugins'].size - 1
 
-  cjoc_install_plugin "update/install #{plugin_name}" do
-    version plugin_ver
-    plugin plugin_name.to_s
-    plugin_source plugin_url
-    plugin_path '/var/lib/cloudbees-core-cm/plugins'
-    restart jenkins_restart
+    cjoc_install_plugin "update/install #{plugin_name}" do
+      version plugin_ver
+      plugin plugin_name.to_s
+      plugin_source plugin_url
+      plugin_path '/var/lib/cloudbees-core-cm/plugins'
+      restart jenkins_restart
+    end
   end
+rescue
+  puts '*** Problem installing/updating plugins. Jenkins may be unavailable or not configured properly.'
 end

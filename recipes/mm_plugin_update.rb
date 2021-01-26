a2b0ec080fd3dc251d8355e9ab184e7c403f645b
Tim Hosey <thosey@cloudbees.com>
2021-01-26 11:05:47 -0800

    added todo to restart jenkins on plugin update
    Signed-off-by: Tim Hosey <thosey@cloudbees.com>

--- recipes/mm_plugin_update.rb
+++ recipes/mm_plugin_update.rb
@@ -5,12 +5,13 @@ begin
     plugin_url = u[1][:source]
     jenkins_restart = index == node['master_plugins'].size - 1
 
+    # TODO: Detect if the plugins were updated or not so we can restart Jenkins
+
     cjoc_install_plugin "update/install #{plugin_name}" do
       version plugin_ver
       plugin plugin_name.to_s
       plugin_source plugin_url
       plugin_path '/var/lib/cloudbees-core-cm/plugins'
-      restart jenkins_restart
     end
   end
 rescue

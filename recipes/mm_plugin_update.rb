3fd4d8a22c607dba7b357ad5f119533ff30a9d29
Tim Hosey <thosey@cloudbees.com>
2021-01-26 11:11:43 -0800

    added some error handling and testing for plugin install
    Signed-off-by: Tim Hosey <thosey@cloudbees.com>

--- recipes/mm_plugin_update.rb
+++ recipes/mm_plugin_update.rb
@@ -1,19 +1,12 @@
-begin
-  node['master_plugins'].each_with_index do |u, index|
-    plugin_name = u[1][:name]
-    plugin_ver = u[1][:version]
-    plugin_url = u[1][:source]
-    jenkins_restart = index == node['master_plugins'].size - 1
+node['master_plugins'].each_with_index do |u, index|
+  plugin_name = u[1][:name]
+  plugin_ver = u[1][:version]
+  plugin_url = u[1][:source]
 
-    # TODO: Detect if the plugins were updated or not so we can restart Jenkins
-
-    cjoc_install_plugin "update/install #{plugin_name}" do
-      version plugin_ver
-      plugin plugin_name.to_s
-      plugin_source plugin_url
-      plugin_path '/var/lib/cloudbees-core-cm/plugins'
-    end
+  cjoc_install_plugin "update/install #{plugin_name}" do
+    version plugin_ver
+    plugin plugin_name.to_s
+    plugin_source plugin_url
+    plugin_path '/var/lib/cloudbees-core-cm/plugins'
   end
-rescue
-  puts '*** Problem installing/updating plugins. Jenkins may be unavailable or not configured properly.'
 end
--- resources/install_plugin.rb
+++ resources/install_plugin.rb
@@ -2,43 +2,61 @@ property :version, String, required: true
 property :plugin, String, required: true
 property :plugin_source, String, required: true
 property :plugin_path, String, default: '/var/lib/cloudbees-core-oc/plugins'
-property :restart, [true, false], default: false
 
 # /var/lib/cloudbees-core-oc/plugins
 
 jenkins = nil
 
 action :install_plugin do
-  # Install gem into Chef
-  chef_gem 'jenkins_api_client' do
-    compile_time true
-  end
+  begin
+    # Install gem into Chef
+    chef_gem 'jenkins_api_client' do
+      compile_time true
+    end
 
-  # Requiring YAML so I don't have to keep username/pass in git
-  require 'yaml'
+    # Requiring YAML so I don't have to keep username/pass in git
+    require 'yaml'
 
-  # Workaround to test without putting creds on github
-  `test -f '/tmp/jenkins_api.yml'`
-  creds = $CHILD_STATUS.success? ? YAML.safe_load(IO.read('/tmp/jenkins_api.yml')) : YAML.safe_load(IO.read('jenkins_api.yml'))
+    # Workaround to test without putting creds on github
+    `test -f '/tmp/jenkins_api.yml'`
+    creds = $CHILD_STATUS.success? ? YAML.safe_load(IO.read('/tmp/jenkins_api.yml')) : YAML.safe_load(IO.read('jenkins_api.yml'))
 
-  require 'jenkins_api_client'
-  jenkins = JenkinsApi::Client.new(
-    server_url: creds['server'],
-    username: creds['username'],
-    password: creds['password']
-  )
+    require 'jenkins_api_client'
+    jenkins = JenkinsApi::Client.new(
+      server_url: creds['server'],
+      username: creds['username'],
+      password: creds['password']
+    )
 
-  puts "\n*** PLUGIN INSTALL: Waiting for Jenkins API to be ready..."
-  jenkins.system.wait_for_ready
+    # Test to see if Jenkins is even available
+    server_root = jenkins.get_root
+    server_date = jenkins.get_server_date
 
-  info = jenkins.plugin.list_installed
-  if info.key?(new_resource.plugin)
-    upgrade = info[new_resource.plugin] < new_resource.version
-    downgrade = info[new_resource.plugin] > new_resource.version
-    if upgrade || downgrade
-      # upgrade or downgrade
-      verb = upgrade ? 'Upgrading' : 'Downgrading'
-      puts "\n*** PLUGIN INSTALL: #{verb} plugin #{new_resource.plugin} to #{new_resource.version}"
+    puts "\n*** Server Info: Root [#{server_root}] / Date [#{server_date}]"
+
+    puts "\n*** PLUGIN INSTALL: Waiting for Jenkins API to be ready..."
+    jenkins.system.wait_for_ready
+
+    info = jenkins.plugin.list_installed
+    if info.key?(new_resource.plugin)
+      upgrade = info[new_resource.plugin] < new_resource.version
+      downgrade = info[new_resource.plugin] > new_resource.version
+      if upgrade || downgrade
+        # upgrade or downgrade
+        verb = upgrade ? 'Upgrading' : 'Downgrading'
+        puts "\n*** PLUGIN INSTALL: #{verb} plugin #{new_resource.plugin} to #{new_resource.version}"
+        remote_file "#{new_resource.plugin_path}/#{new_resource.plugin}.jpi" do
+          source new_resource.plugin_source.to_s
+          owner 'root'
+          group 'root'
+          mode '0755'
+          action :create
+          notifies :run, 'ruby_block[restart Jenkins]', :delayed
+        end
+      end
+    else
+      # install
+      puts "\n*** PLUGIN INSTALL: Installing new plugin #{new_resource.plugin} to #{new_resource.version}"
       remote_file "#{new_resource.plugin_path}/#{new_resource.plugin}.jpi" do
         source new_resource.plugin_source.to_s
         owner 'root'
@@ -48,28 +66,20 @@ action :install_plugin do
         notifies :run, 'ruby_block[restart Jenkins]', :delayed
       end
     end
-  else
-    # install
-    puts "\n*** PLUGIN INSTALL: Installing new plugin #{new_resource.plugin} to #{new_resource.version}"
-    remote_file "#{new_resource.plugin_path}/#{new_resource.plugin}.jpi" do
-      source new_resource.plugin_source.to_s
-      owner 'root'
-      group 'root'
-      mode '0755'
-      action :create
-      notifies :run, 'ruby_block[restart Jenkins]', :delayed
-    end
-  end
-  # Restart...
-  ruby_block 'restart Jenkins' do
-    block do
-      if new_resource.restart
-        puts "\n*** PLUGIN INSTALL: Restarting Jenkins when all jobs have finished..."
-        jenkins.system.restart(false)
-      else
-        puts "\n*** PLUGIN INSTALL: Skipping restart on this plugin."
+    # Restart...
+    # TODO: Detect if the plugins were updated or not so we can restart Jenkins
+    ruby_block 'restart Jenkins' do
+      block do
+        if restart_jenkins
+          puts "\n*** PLUGIN INSTALL: Restarting Jenkins when all jobs have finished..."
+          jenkins.system.restart(false)
+        else
+          puts "\n*** PLUGIN INSTALL: Skipping restart on this plugin."
+        end
       end
+      action :nothing
     end
-    action :nothing
+  rescue
+    puts "\n*** Problem installing/updating plugins. Jenkins may be unavailable or not configured properly."
   end
 end

efc33760173d5440c5f296aa72d599e704e0f0cc
Tim Hosey <thosey@cloudbees.com>
2021-03-01 15:05:39 -0800

    changed target ver to 2.263.2.3

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,10 +2,10 @@
 default['chef_client']['interval'] = 1800
 
 # CJOC Version
-default['cjoc']['target_version'] = '2.263.1.2'
+default['cjoc']['target_version'] = '2.263.2.3'
 
 # CJMC Version
-default['cjcm']['target_version'] = '2.263.1.2'
+default['cjcm']['target_version'] = '2.263.2.3'
 
 # CJCM CasC path
 default['cjcm']['casc_path'] = '/etc/cbci/casc'
